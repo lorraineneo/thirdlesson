7439c7971a3dc67682d23786afcc01dafb3bad6c
lorraineneo <lorraineneo@gmail.com>
2015-07-28 09:55:25 +0800

    Update urls.py

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView, DetailView
 from notes.models import Note
 
 urlpatterns = patterns('',
-    url(r'^listall/$', ListView.as_view(model=Note)),
+    url(r'^listall/$', ListView.as_view(model=Note), name='listall'),
     url(r'^list/(?P<folder>.*)$', views.NoteList.as_view(), name='notes_list'),
     url(r'^add/$', views.NoteCreate.as_view(), name='note_add'),
     url(r'^note/(?P<pk>\d+)/edit/$', views.NoteUpdate.as_view(),  name='note_update'),
